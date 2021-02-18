d91626a32408915fc5764cad8883bf9363fa30ac
demidraksin <greatraksin@icloud.com>
2021-02-18 11:49:03 +0300

    Adding routing🌳

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+from routes import *
 
 if __name__ == '__main__':
     app.run()
--- routes.py
+++ routes.py
@@ -1,1 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
+from app import app
+
+
+@app.route('/')
+def hello_world():
+    return render_template('base.html', title='Main Page')
