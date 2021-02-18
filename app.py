53dbbe42534141ffb31517b090c531343351c655
demidraksin <greatraksin@icloud.com>
2021-02-18 11:21:15 +0300

    🔥Initial commit🔥

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,5 @@
+from app import app
+
+
+class Config:
+    SECRET_KEY = 'try-to-guess'
